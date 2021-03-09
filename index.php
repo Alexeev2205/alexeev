9eee755e18a3e3353cc0572d214d1c1481010b35
Denis Alexeev <bigden2205@gmail.com>
2021-03-09 14:14:48 +0300

    1.0

---
+++ index.php
@@ -0,0 +1,35 @@
+﻿// вариант 2 Алексеев Денис
+<?php
+
+class A
+{
+
+}
+
+class B extends Line
+{
+    public function __construct($a)
+    {
+        $this->a = $a;
+    }
+
+    protected $a;
+}
+
+class C extends Square
+{
+    public function __construct($a, $b)
+    {
+        $this->b = $b;
+        parent::__construct($a);
+    }
+
+    protected $b;
+}
+
+$a1 = new Line();
+$a2 = new Line();
+$a3 = new Line();
+$b4 = new Square($a1);
+$c5 = new C($b4, $a2, $a3);
+var_dump ($c5);
